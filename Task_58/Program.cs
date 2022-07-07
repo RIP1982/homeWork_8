0a7994419b061a6c3e0d907d2bd4ecbf9cd70f35
RIP1982 <perovsky@list.ru>
2022-07-07 20:58:36 +0400

    Solution task 58

---
+++ Task_58/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+
+Console.Clear();
+
+int m = Input("Введите количество строк первого массива: ");
+int n = Input("Введите количество столбцов первого массива, строк второго массива: ");
+int l = Input("Введите количество столбцов второго массива: ");
+
+int[,] firstArray = new int[m, n];
+int[,] secondArray = new int[n, l];
+
+for (int i = 0; i < firstArray.GetLength(0); i++)
+{
+    for (int j = 0; j < firstArray.GetLength(1); j++)
+    {
+        firstArray[i, j] = new Random().Next(1, 10);
+    }
+}
+
+for (int i = 0; i < secondArray.GetLength(0); i++)
+{
+    for (int j = 0; j < secondArray.GetLength(1); j++)
+    {
+        secondArray[i, j] = new Random().Next(1, 10);
+    }
+}
+int[,] thirdArray = new int[m, l];
+
+for (int i = 0; i < firstArray.GetLength(0); i++)
+{
+    for (int j = 0; j < secondArray.GetLength(1); j++)
+    {
+        for (int k = 0; k < firstArray.GetLength(1); k++)
+        {
+            thirdArray[i, j] += firstArray[i, k] * secondArray[k, j];
+        }
+    }
+    Console.WriteLine();
+}
+
+PrintArray(firstArray);
+Console.WriteLine("*");
+PrintArray(secondArray);
+Console.WriteLine("=");
+PrintArray(thirdArray);
+
+int Input(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
